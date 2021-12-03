daa7d1f64ac281a640929860c28f338bb2431518
fajllo <61871978+fajllo@users.noreply.github.com>
2021-12-03 10:46:39 +0100

    additional db configuration

--- code/app.py
+++ code/app.py
@@ -9,6 +9,8 @@ from database import db
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'secret key'
 api = Api(app)
